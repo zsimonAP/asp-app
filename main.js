d3b98b65427815012bd66ad893b4e76c54f49fee
zsimonAP <zsimon@associatedpension.com>
2024-08-07 15:57:43 -0400

    not using cached python

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ function createWindow(url) {
     webPreferences: {
       preload: path.join(app.getAppPath(), 'preload.js'),
       nodeIntegration: true,
-      contextIsolation: true // Ensures security
+      contextIsolation: true, // Ensures security
     },
   });
 
@@ -127,15 +127,7 @@ async function startApp() {
 
     log.info(`App path: ${__dirname}`);
 
-    let pythonPath;
-    if (process.env.NODE_ENV === 'production') {
-      // Production path
-      pythonPath = path.join(process.resourcesPath, 'env', 'Scripts', 'python.exe');
-    } else {
-      // Development path
-      pythonPath = path.join(__dirname, 'env', 'Scripts', 'python.exe');
-    }
-
+    const pythonPath = process.env.PYTHONPATH || path.join(process.resourcesPath, 'env', 'Scripts', 'python.exe');
     log.info(`Python path: ${pythonPath}`);
     const serverScriptPath = path.join(process.resourcesPath, 'backend', 'server.py');
     log.info(`Server script path: ${serverScriptPath}`);
